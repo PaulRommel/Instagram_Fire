6c06d1e674f22161b4fd1b292a770ce7c143602a
Paul <paulrommel87@gmail.com>
2026-04-09 19:11:46 +0300

    02. UIColor and Anchor Extension

---
+++ InstagramFire/Utils/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  InstagramFire
+//
+//  Created by Павел Попов on 09.04.2026.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+//    func someRandomMethod() {
+//        
+//    }
+    
+}
