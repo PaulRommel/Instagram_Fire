42cd32ec0e162b98c9f4e6a61fb0afbf68b2a132
Paul <paulrommel87@gmail.com>
2026-04-11 16:46:54 +0300

    03. Firebase SDK and User Registration

--- InstagramFire/ViewController.swift
+++ InstagramFire/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
     
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
         tf.placeholder = "Email"
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
@@ -28,25 +31,31 @@ class ViewController: UIViewController {
         tf.placeholder = "Username"
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
-    let passworTextField: UITextField = {
+    let passwordTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Password"
         tf.isSecureTextEntry = true
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
-    let singUpButton: UIButton = {
+    let signUpButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Sing Up", for: .normal)
+        button.setTitle("Sign Up", for: .normal)
         button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.isEnabled = false
         return button
     }()
     
@@ -55,46 +64,112 @@ class ViewController: UIViewController {
         
         view.addSubview(plusPhotoButton)
         
-        plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
+        plusPhotoButton.anchor(
+            top: view.topAnchor,
+            left: nil,
+            bottom: nil,
+            right: nil,
+            paddingTop: 40,
+            paddingLeft: 0,
+            paddingBottom: 0,
+            paddingRight: 0,
+            width: 140,
+            height: 140
+        )
         
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         setupInputFields()
+    }
     
+    @objc func handleTextInputChange() {
+        let isFormValid =
+            (emailTextField.text?.isEmpty == false) &&
+            (usernameTextField.text?.isEmpty == false) &&
+            (passwordTextField.text?.isEmpty == false)
+        
+        signUpButton.isEnabled = isFormValid
+        signUpButton.backgroundColor = isFormValid
+            ? UIColor.rgb(red: 17, green: 154, blue: 237)
+            : UIColor.rgb(red: 149, green: 204, blue: 244)
+    }
+    
+    @objc func handleSignUp() {
+        guard let email = emailTextField.text, !email.isEmpty else { return }
+        guard let username = usernameTextField.text, !username.isEmpty else { return }
+        guard let password = passwordTextField.text, !password.isEmpty else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Failed to create user:", error.localizedDescription)
+                return
+            }
+            
+            guard let uid = authResult?.user.uid else { return }
+            
+            print("Successfully created user:", uid)
+            print("Username:", username)
+        }
     }
     
     fileprivate func setupInputFields() {
-        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passworTextField, singUpButton ])
-        stackView.distribution = .fillEqually
+        let stackView = UIStackView(arrangedSubviews: [
+            emailTextField,
+            usernameTextField,
+            passwordTextField,
+            signUpButton
+        ])
+        
         stackView.axis = .vertical
         stackView.spacing = 10
+        stackView.distribution = .fillEqually
         
         view.addSubview(stackView)
         
-        stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
+        stackView.anchor(
+            top: plusPhotoButton.bottomAnchor,
+            left: view.leftAnchor,
+            bottom: nil,
+            right: view.rightAnchor,
+            paddingTop: 20,
+            paddingLeft: 40,
+            paddingBottom: 0,
+            paddingRight: 40,
+            width: 0,
+            height: 200
+        )
     }
-    
 }
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
-        
+    func anchor(
+        top: NSLayoutYAxisAnchor?,
+        left: NSLayoutXAxisAnchor?,
+        bottom: NSLayoutYAxisAnchor?,
+        right: NSLayoutXAxisAnchor?,
+        paddingTop: CGFloat,
+        paddingLeft: CGFloat,
+        paddingBottom: CGFloat,
+        paddingRight: CGFloat,
+        width: CGFloat,
+        height: CGFloat
+    ) {
         translatesAutoresizingMaskIntoConstraints = false
         
         if let top = top {
-            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
         }
         
         if let left = left {
-            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
         }
         
         if let bottom = bottom {
-            self.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
         }
         
         if let right = right {
-            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
         }
         
         if width != 0 {
@@ -104,6 +179,5 @@ extension UIView {
         if height != 0 {
             heightAnchor.constraint(equalToConstant: height).isActive = true
         }
-        
     }
 }
